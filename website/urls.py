ef101e9f2eadde55377d60b2842c477eb72a0796
Assentt <tubbeethan@gmail.com>
2023-01-26 14:18:45 -0800

    add certification paths/urls

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index), 
-    path('resume/', views.resume_pdf, name='resume')
+    path('resume/', views.resume_pdf, name='resume'),
+    path('python3/', views.python3_pdf, name='python3'),
+    path('javascript/', views.javascript_pdf, name='javascript'),
+    path('html/', views.html_pdf, name='html'),
+    path('css/', views.css_pdf, name='css'),
+    path('gitgithub/', views.gitgithub_pdf, name='gitgithub'),
 ]
--- website/views.py
+++ website/views.py
@@ -14,3 +14,38 @@ def resume_pdf(request):
     
     except FileNotFoundError:
         raise Http404()
+    
+def python3_pdf(request):
+    try:
+        return FileResponse(open('website/static/website/assets/pdf/Codecademy_Python3.pdf', 'rb'), content_type='application/pdf')
+    
+    except FileNotFoundError:
+        raise Http404()
+
+def javascript_pdf(request):
+    try:
+        return FileResponse(open('website/static/website/assets/pdf/Codecademy_IntermediateJavaScript.pdf', 'rb'), content_type='application/pdf')
+    
+    except FileNotFoundError:
+        raise Http404()
+    
+def html_pdf(request):
+    try:
+        return FileResponse(open('website/static/website/assets/pdf/Codecademy_HTML.pdf', 'rb'), content_type='application/pdf')
+    
+    except FileNotFoundError:
+        raise Http404()
+    
+def css_pdf(request):
+    try:
+        return FileResponse(open('website/static/website/assets/pdf/Codecademy_CSS.pdf', 'rb'), content_type='application/pdf')
+    
+    except FileNotFoundError:
+        raise Http404()
+    
+def gitgithub_pdf(request):
+    try:
+        return FileResponse(open('website/static/website/assets/pdf/Codecademy_Git&GitHub.pdf', 'rb'), content_type='application/pdf')
+    
+    except FileNotFoundError:
+        raise Http404()
